1d37394f891ecc0bc6bed2549c003eae0b2459f6
Alper <alper.tasci@hotmail.com>
2022-04-18 20:02:16 +0300

    E-Trade(Entity Framework) 4 | Added GetProductById(), GetProductByUnitPrice() functions using LINQ

--- c-sharp/E-Trade(Entity Framework)/ProductDal.cs
+++ c-sharp/E-Trade(Entity Framework)/ProductDal.cs
@@ -16,11 +16,23 @@ namespace E_Trade_Entity_Framework_
             using (ETradeContext eTradeContext = new ETradeContext())
                 return eTradeContext.Products.ToList();
         }
+
+        public Product GetProductById(int id)
+        {
+            using (ETradeContext eTradeContext = new ETradeContext())
+                return eTradeContext.Products.FirstOrDefault(p => p.Id == id);
+        }
         public List<Product> GetProductByName(string name)
         {
             using (ETradeContext eTradeContext = new ETradeContext())
                 return eTradeContext.Products.Where(product => product.Name.Contains(name)).ToList();
         }
+        public List<Product> GetProductByUnitPrice(decimal min, decimal max)
+        {
+            using (ETradeContext eTradeContext = new ETradeContext())
+                return eTradeContext.Products.Where(product => product.UnitPrice >= min && product.UnitPrice <= max).ToList();
+        }
+        
         public void AddProduct(Product product)
         {
             using (ETradeContext eTradeContext = new ETradeContext())
