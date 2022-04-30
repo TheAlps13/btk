c274650862774b6135dddc0cc25c0937b53fd63f
Alper <thealps13@gmail.com>
2022-04-30 14:39:55 +0300

    stock-control-example(using events) 1 | Initial commit

---
+++ c-sharp/stock-control-example(using events)/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stock_control_example_using_events_
+{
+    
+    public class Product
+    {
+        private int _stock;
+        public Product(int stock)
+        {
+            _stock = stock;
+        }
+        public delegate void StockControl();
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+        public int StockAmount 
+        {
+            get { return _stock; }
+            set 
+            {
+                _stock = value;
+                if(_stock < 15 && StockControlEvent != null)
+                {
+                    StockControlEvent();
+                }
+            } 
+        }
+        public void Sell(int amount)
+        {
+           StockAmount -= amount;
+            Console.WriteLine($"{ProductName} Stock Amount{StockAmount}");
+        }
+    }
+}
---
+++ c-sharp/stock-control-example(using events)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace stock_control_example_using_events_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product hdd = new Product(50);
+            hdd.ProductName = "Hard Disk";
+
+            Product cpu =  new Product(50);
+            cpu.ProductName = "CPU";
+            cpu.StockControlEvent += CpuStockControlEvent;
+
+            do
+            {
+                hdd.Sell(10);
+                cpu.Sell(10);
+            }
+            while (Console.ReadLine() != "0");
+        }
+
+        private static void CpuStockControlEvent()
+        {
+            Console.WriteLine("CPU is running out of stock");
+        }
+    }
+}
