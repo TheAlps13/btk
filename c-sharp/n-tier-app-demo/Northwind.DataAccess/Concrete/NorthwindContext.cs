60389143067d5967aed545e4decea7407e48d63a
Alper <thealps13@gmail.com>
2022-05-06 20:20:43 +0300

    N-Tier App Demo 2 | Added data access layer

---
+++ c-sharp/n-tier-app-demo/Northwind.DataAccess/Concrete/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.DataAccess.Concrete
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
