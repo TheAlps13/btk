d7d9e0f0ea96afbd6dc8323dd524d87f9219c4b0
Alper <thealps13@gmail.com>
2022-06-13 08:16:02 +0300

    Design Patterns 4 | Implemented factory method

---
+++ c-sharp/DesignPatterns/factory-method-example/CustomerManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_method_example
+{
+    public class CustomerManager
+    {
+        public void Save()
+        {
+            ILogger logger = LoggerFactory.CreateInstance("Joker");
+            logger.Log("Saved !");
+        }
+    }
+}
---
+++ c-sharp/DesignPatterns/factory-method-example/ILogger.cs
@@ -0,0 +1,7 @@
+﻿namespace factory_method_example
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+}
