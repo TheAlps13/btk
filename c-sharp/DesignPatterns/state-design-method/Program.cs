f68667d53085b178dfba84ea93d7a61e5c6041d6
Alper <alper.tasci@hotmail.com>
2022-08-19 14:35:48 +0300

    Design Patterns 18 | Implemented state design method

---
+++ c-sharp/DesignPatterns/state-design-method/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace state_design_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseContext context = new DatabaseContext();
+            context.SetState(new ModifiedState());
+            context.SaveChanges();
+            context.SetState(new DeletedState());
+            context.SaveChanges();
+        }
+    }
+
+    public interface DatabaseState
+    {
+        public void SaveChanges(DatabaseContext context);
+    }
+
+    public class DatabaseContext
+    {
+        private DatabaseState _state;
+
+        public DatabaseContext()
+        {
+            _state = new ModifiedState();
+        }
+
+        public void SetState(DatabaseState state)
+        {
+            _state = state;
+        }
+
+        public DatabaseState GetState()
+        {
+            return _state;
+        }
+
+        public void SaveChanges()
+        {
+            _state.SaveChanges(this);
+        }
+    }
+
+    public class ModifiedState : DatabaseState
+    {
+        public void SaveChanges(DatabaseContext context)
+        {
+            Console.WriteLine("Database record modified!");
+        }
+    }
+
+    public class DeletedState : DatabaseState
+    {
+        public void SaveChanges(DatabaseContext context)
+        {
+            Console.WriteLine("Database record deleted!");
+        }
+    }
+}
